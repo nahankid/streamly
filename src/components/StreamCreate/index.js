5cad89ad9034c1bdfd9a8336536335309753b6b4
Sharad Gupta <nahankid@gmail.com>
2019-01-28 13:35:49 +0530

    Add Create Stream Form

--- src/components/StreamCreate/index.js
+++ src/components/StreamCreate/index.js
@@ -1,9 +1,86 @@
 import React from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { reduxForm, Field } from "redux-form";
 
 class StreamCreate extends React.Component {
+  renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+    <div className={`field ${touched && (error || warning) ? "error" : ""}`}>
+      <label>{label}</label>
+      <div>
+        <input {...input} placeholder={label} type={type} />
+        {touched &&
+          ((error && <span style={{ color: "red" }}>{error}</span>) ||
+            (warning && <span>{warning}</span>))}
+      </div>
+    </div>
+  );
+
+  onSubmit = values => {
+    console.log(values);
+  };
+
   render() {
-    return <div>StreamCreate</div>;
+    const { handleSubmit, pristine, reset, submitting } = this.props;
+    return (
+      <form className="ui form" onSubmit={handleSubmit(this.onSubmit)}>
+        <Field
+          name="title"
+          type="text"
+          component={this.renderField}
+          label="Title"
+        />
+        <Field
+          name="description"
+          type="text"
+          component={this.renderField}
+          label="Description"
+        />
+        <div>
+          <button
+            className="ui right floated primary button"
+            type="submit"
+            disabled={submitting}
+          >
+            Submit
+          </button>
+          <button
+            className="ui left floated button"
+            type="button"
+            disabled={pristine || submitting}
+            onClick={reset}
+          >
+            Reset
+          </button>
+        </div>
+      </form>
+    );
   }
 }
 
-export default StreamCreate;
+const validate = values => {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Required";
+  }
+
+  if (!values.description) {
+    errors.description = "Required";
+  }
+
+  return errors;
+};
+
+const mapStateToProps = state => ({});
+
+const withForm = reduxForm({ form: "createStream", validate });
+const withConnect = connect(
+  mapStateToProps,
+  {}
+);
+
+export default compose(
+  withForm,
+  withConnect
+)(StreamCreate);
--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
+
 import googleAuthReducer from "./auth/GoogleAuth/reducer";
 
 const reducers = combineReducers({
+  form: formReducer,
   googleAuth: googleAuthReducer
 });
 
